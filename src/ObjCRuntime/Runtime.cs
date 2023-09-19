a4ec10f4fad8b744b33e66b6c6ae05bce9e4a507
Dustin Wojciechowski <dustin.wojciechowski@microsoft.com>
2023-09-18 21:45:30 -0700

    Whitespace

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -54,10 +54,8 @@ namespace ObjCRuntime {
 		static object lock_obj;
 		static IntPtr NSObjectClass;
 		static bool initialized;
-
 		static bool useCFNetworkHandler = false;
 		static bool useNSUrlSessionHandler = false;
-
 
 		internal static IntPtrEqualityComparer IntPtrEqualityComparer;
 		internal static TypeEqualityComparer TypeEqualityComparer;
@@ -290,7 +288,9 @@ namespace ObjCRuntime {
 		}
 
 		internal static bool UseCFNetworkHandler => useCFNetworkHandler;
+
 		internal static bool UseNSUrlSessionHandler => useNSUrlSessionHandler;
+
 		internal static bool Initialized {
 			get { return initialized; }
 		}

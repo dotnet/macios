560e473e46b489c3968aa918de085e8568b7c81b
Manuel de la Pena <mandel@microsoft.com>
2025-03-10 17:25:35 -0400

    Update src/rgen/Microsoft.Macios.Generator/Emitters/BindingSyntaxFactory.Runtime.cs
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- src/rgen/Microsoft.Macios.Generator/Emitters/BindingSyntaxFactory.Runtime.cs
+++ src/rgen/Microsoft.Macios.Generator/Emitters/BindingSyntaxFactory.Runtime.cs
@@ -275,7 +275,7 @@ static partial class BindingSyntaxFactory {
 	}
 
 	/// <summary>
-	/// Generates the correct caall to the To* methods in the NSNumber class that converts a NativeHandle
+	/// Generates the correct call to the To* methods in the NSNumber class that converts a NativeHandle
 	/// to the return type of the method/property.
 	/// </summary>
 	/// <param name="returnType">The return method of the method/property.</param>

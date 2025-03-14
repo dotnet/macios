4dca75c4136f83f2b9733056d463d6100a9567a9
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-03-14 08:18:33 -0400

    [Rgen] Generalize NSArray.FromNSObject to later use it for other code generation.
    The NSArray.FromNSObject is used in several parts of the code
    generation, for example trampolines will use it too. We generalize the
    call so that we can easily do function composition to generate the code.

--- tests/rgen/Microsoft.Macios.Generator.Tests/Emitters/BindingSyntaxFactoryRuntimeTests.cs
+++ tests/rgen/Microsoft.Macios.Generator.Tests/Emitters/BindingSyntaxFactoryRuntimeTests.cs
@@ -407,5 +407,34 @@ public class BindingSyntaxFactoryRuntimeTests {
 		var str = declaration.ToString ();
 		Assert.Equal (expectedDeclaration, declaration.ToFullString ());
 	}
+	
+	class TestDataNSArrayFromNSObjects : IEnumerable<object []> {
+		public IEnumerator<object []> GetEnumerator ()
+		{
+			yield return [
+				ImmutableArray.Create (
+					Argument (IdentifierName ("arg1"))),
+				"NSArray.FromNSObjects (arg1)"
+			];
+
+			yield return [
+				ImmutableArray.Create (
+					Argument (IdentifierName ("arg1")),
+					Argument (IdentifierName ("arg2")),
+					Argument (IdentifierName ("arg3"))),
+				"NSArray.FromNSObjects (arg1, arg2, arg3)"
+			];
+		}
+
+		IEnumerator IEnumerable.GetEnumerator () => GetEnumerator ();
+	}
+
+	[Theory]
+	[ClassData (typeof (TestDataNSArrayFromNSObjects))]
+	void NSArrayFromNSObjectsTests (ImmutableArray<ArgumentSyntax> arguments, string expectedDeclaration)
+	{
+		var declaration = NSArrayFromNSObjects (arguments);
+		Assert.Equal (expectedDeclaration, declaration.ToFullString ());
+	}
 
 }

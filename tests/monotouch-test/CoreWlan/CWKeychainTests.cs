f8d91ed38429f8595a4b37ec09fc7b882188a87b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-03-17 10:42:07 +0100

    Use 'None' keychain.

--- tests/monotouch-test/CoreWlan/CWKeychainTests.cs
+++ tests/monotouch-test/CoreWlan/CWKeychainTests.cs
@@ -23,7 +23,7 @@ namespace MonoTouchFixtures.CoreWlan {
 		[SetUp]
 		public void SetUp ()
 		{
-			domain = CWKeychainDomain.System;
+			domain = CWKeychainDomain.None;
 			// always return a new one so that test do not mess with eachother
 			ssid = NSData.FromString (Guid.NewGuid ().ToString ());
 		}
@@ -31,35 +31,42 @@ namespace MonoTouchFixtures.CoreWlan {
 		[Test]
 		public void TryFindWiFiEAPIdentityMissingTest ()
 		{
+			Assert.Multiple (() => {
 			Assert.False (CWKeychain.TryFindWiFiEAPIdentity (domain, ssid, out var secIdentity), "A");
 			Assert.IsNull (secIdentity, "A Identity");
 
 			Assert.False (CWKeychain.TryFindWiFiEAPIdentity (domain, ssid, out secIdentity, out var status), "B");
 			Assert.IsNull (secIdentity, "B Identity");
 			Assert.AreEqual (SecStatusCode.ItemNotFound, (SecStatusCode) status, "Status B");
+		});
 		}
 
 		[Test]
 		public void TryDeleteWiFiEAPUsernameAndPasswordMissingTest ()
 		{
+			Assert.Multiple (() => {
 			Assert.False (CWKeychain.TryDeleteWiFiEAPUsernameAndPassword (domain, ssid), "A");
 
 			Assert.False (CWKeychain.TryDeleteWiFiEAPUsernameAndPassword (domain, ssid, out var status));
 			Assert.AreEqual (SecStatusCode.ItemNotFound, (SecStatusCode) status, "Status B");
+		});
 		}
 
 		[Test]
 		public void TryDeleteWiFiPasswordMissingTest ()
 		{
-			Assert.True (CWKeychain.TryDeleteWiFiPassword (domain, ssid), "A");
+			Assert.Multiple (() => {
+			Assert.False (CWKeychain.TryDeleteWiFiPassword (domain, ssid), "A");
 
-			Assert.True (CWKeychain.TryDeleteWiFiPassword (domain, ssid, out var status), "B");
-			Assert.AreEqual (SecStatusCode.Success, (SecStatusCode) status, "Status B");
+			Assert.False (CWKeychain.TryDeleteWiFiPassword (domain, ssid, out var status), "B");
+			Assert.AreEqual (SecStatusCode.Param, (SecStatusCode) status, "Status B");
+		});
 		}
 
 		[Test]
 		public void TryFindWiFiEAPUsernameAndPasswordMissingTest ()
 		{
+			Assert.Multiple (() => {
 			Assert.False (CWKeychain.TryFindWiFiEAPUsernameAndPassword (domain, ssid, out string username, out string password), "A");
 			Assert.IsNull (username, "A username");
 			Assert.IsNull (password, "A password");
@@ -68,59 +75,68 @@ namespace MonoTouchFixtures.CoreWlan {
 			Assert.IsNull (username, "B username");
 			Assert.IsNull (password, "B password");
 			Assert.AreEqual (SecStatusCode.ItemNotFound, (SecStatusCode) status, "Status B");
+		});
 		}
 
 		[Test]
 		public void TryFindWiFiPasswordMissingTest ()
 		{
+			Assert.Multiple (() => {
 			Assert.False (CWKeychain.TryFindWiFiPassword (domain, ssid, out string password), "A");
 			Assert.IsNull (password, "A password");
 
 			Assert.False (CWKeychain.TryFindWiFiPassword (domain, ssid, out password, out var status), "B");
 			Assert.IsNull (password, "B password");
-			Assert.AreEqual (SecStatusCode.ItemNotFound, (SecStatusCode) status, "Status B");
+			Assert.AreEqual (SecStatusCode.Param, (SecStatusCode) status, "Status B");
+		});
 		}
 
 		[Test]
 		public void TrySetWiFiEAPIdentityTest ()
 		{
+			Assert.Multiple (() => {
 			// false because the ssid is not present
 			var identity = IdentityTest.GetIdentity ();
-			Assert.False (CWKeychain.TrySetWiFiEAPIdentity (domain, ssid, identity), "A");
+			Assert.True (CWKeychain.TrySetWiFiEAPIdentity (domain, ssid, identity), "A");
 
-			Assert.False (CWKeychain.TrySetWiFiEAPIdentity (domain, ssid, identity, out var status), "B");
-			Assert.AreEqual (SecStatusCode.Allocate, (SecStatusCode) status, "Status B");
+			Assert.True (CWKeychain.TrySetWiFiEAPIdentity (domain, ssid, identity, out var status), "B");
+			Assert.AreEqual (SecStatusCode.Success, (SecStatusCode) status, "Status B");
 
 			// remove it to clean behind
 			Assert.False (CWKeychain.TryDeleteWiFiEAPUsernameAndPassword (domain, ssid), "C");
 
 			Assert.False (CWKeychain.TryDeleteWiFiEAPUsernameAndPassword (domain, ssid, out status), "D");
 			Assert.AreEqual (SecStatusCode.ItemNotFound, (SecStatusCode) status, "Status D");
+		});
 		}
 
 		[Test]
 		public void TrySetWiFiEAPUsernameAndPasswordTest ()
 		{
-			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", "test"), "Both present A");
-			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", "test", out var status), "Both present B");
-			Assert.AreEqual (SecStatusCode.Allocate, (SecStatusCode) status, "Both present B Status");
+			Assert.Multiple (() => {
+			Assert.True (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", "test"), "Both present A");
+			Assert.True (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", "test", out var status), "Both present B");
+			Assert.AreEqual (SecStatusCode.Success, (SecStatusCode) status, "Both present B Status");
 
-			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", null), "Null pwd A");
-			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", null, out status), "Null pwd B");
-			Assert.AreEqual (SecStatusCode.Allocate, (SecStatusCode) status, "Null pwd B Status");
+			Assert.True (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", null), "Null pwd A");
+			Assert.True (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, "mandel", null, out status), "Null pwd B");
+			Assert.AreEqual (SecStatusCode.Success, (SecStatusCode) status, "Null pwd B Status");
 
 			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, null, "test"), "Null user A");
 			Assert.False (CWKeychain.TrySetWiFiEAPUsernameAndPassword (domain, ssid, null, "test", out status), "Null user B");
 			Assert.AreEqual (SecStatusCode.Param, (SecStatusCode) status, "Null user B Status");
+		});
 		}
 
 		[Test]
 		public void TrySetWiFiPasswordTest ()
 		{
-			Assert.True (CWKeychain.TrySetWiFiPassword (domain, ssid, "password"), "A");
+			Assert.Multiple (() => {
+			Assert.False (CWKeychain.TrySetWiFiPassword (domain, ssid, "password"), "A");
 
-			Assert.True (CWKeychain.TrySetWiFiPassword (domain, ssid, "password", out var status), "B");
-			Assert.AreEqual (SecStatusCode.Success, (SecStatusCode) status, "Status B");
+			Assert.False (CWKeychain.TrySetWiFiPassword (domain, ssid, "password", out var status), "B");
+			Assert.AreEqual (SecStatusCode.Param, (SecStatusCode) status, "Status B");
+		});
 		}
 
 	}

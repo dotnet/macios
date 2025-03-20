171e434cbee0b0913797cc659fcc0d324c18e812
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-03-20 09:12:24 +0100

    [devops] Adjust a few things with the xamarin-macios keychain
    * Disable keychain autolock.
    * Disable the xamarin-macios keychain before running macOS tests.

--- tools/devops/automation/scripts/install-qa-provisioning-profiles.sh
+++ tools/devops/automation/scripts/install-qa-provisioning-profiles.sh
@@ -67,8 +67,8 @@ security create-keychain -p "$(cat "$KEYCHAIN_PWD_FILE")" "$KEYCHAIN.keychain"
 echo "${BLUE}Unlocking keychain${CLEAR}"
 security unlock-keychain -p "$(cat "$KEYCHAIN_PWD_FILE")" "$KEYCHAIN_FILE"
 
-echo "${BLUE}Increasing keychain unlock timeout for '${WHITE}$KEYCHAIN_FILE${BLUE} to 6 hours${CLEAR}"
-security set-keychain-settings -lut 21600 "$KEYCHAIN_FILE"
+echo "${BLUE}Disable keychain autolock for '${WHITE}$KEYCHAIN_FILE${BLUE}${CLEAR}"
+security set-keychain-settings "$KEYCHAIN_FILE"
 
 echo "${BLUE}Adding certificate(s) to keychain:${CLEAR}"
 for cert in provisioning-profiles/certificates-and-profiles/*.cer; do

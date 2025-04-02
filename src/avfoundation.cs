dfc11517cb642c0dcd9914960bf3a10be845b6bf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-04-02 14:54:39 +0200

    [AVFoundation] Add a default ctor for AVRouteDetector. Fixes #10139.
    It doesn't work in the simulator, but it works on device.
    
    Fixes https://github.com/dotnet/macios/issues/10139.

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -20711,7 +20711,6 @@ namespace AVFoundation {
 	}
 
 	[MacCatalyst (13, 1)]
-	[DisableDefaultCtor]
 	[BaseType (typeof (NSObject))]
 	interface AVRouteDetector {
 		/// <summary>To be added.</summary>

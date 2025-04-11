ecb1bdde70dd9220f76f2e067812c87088f99ea3
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-04-11 17:47:42 -0400

    [RGen] Keep track if the element of an array is a INativeObject.
    This is needed for trampolines that are dealing with INativeObjects that
    are not inheriting from NSObject.

--- src/rgen/Microsoft.Macios.Generator/DataModel/TypeInfo.cs
+++ src/rgen/Microsoft.Macios.Generator/DataModel/TypeInfo.cs
@@ -128,6 +128,11 @@ readonly partial struct TypeInfo : IEquatable<TypeInfo> {
 	/// Returns, if the type is an array, if its elements are a wrapped object from the objc world.
 	/// </summary>
 	public bool ArrayElementTypeIsWrapped { get; init; }
+	
+	/// <summary>
+	/// Returns, if the type is an array, if its elements implement the INativeObject interface.
+	/// </summary>
+	public bool ArrayElementIsINativeObject { get; init; }
 
 	readonly bool isNSObject = false;
 
@@ -259,6 +264,7 @@ readonly partial struct TypeInfo : IEquatable<TypeInfo> {
 			IsArray = true;
 			ArrayElementType = arraySymbol.ElementType.SpecialType;
 			ArrayElementTypeIsWrapped = arraySymbol.ElementType.IsWrapped ();
+			ArrayElementIsINativeObject = arraySymbol.ElementType.IsINativeObject ();
 		}
 
 		// try to get the named type symbol to have more educated decisions

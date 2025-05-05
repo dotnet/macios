56d0737a4e1fd4d4f84a3f04d40c02c5943f1161
Mauro Agnoletti <maagno@microsoft.com>
2025-05-05 14:49:25 -0400

    [draft][test] Update Xamarin.Messaging and renamed Xamarin paths

--- msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
+++ msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Messaging.Client;
 
@@ -10,7 +12,14 @@ namespace Xamarin.Messaging.Build {
 			var agent = new BuildAgent (topicGenerator, arguments.Version, arguments.VersionInfo);
 			var runner = new AgentConsoleRunner<BuildAgent> (agent, arguments);
 
-			await runner.RunAsync ().ConfigureAwait (continueOnCapturedContext: false);
+			//Hack to support legacy paths from Windows (likely Dev17 versions)
+			if (MessagingContext.BasePath.Contains("Xamarin")) {
+				var xamarinPath = MessagingContext.BasePath.Substring (0, MessagingContext.BasePath.IndexOf ("Xamarin") + "Xamarin".Length);
+
+				MessagingContext.BuildsPath = Path.Combine (xamarinPath, "mtbs", "builds");
+			}
+
+			await runner.RunAsync (CancellationToken.None).ConfigureAwait (continueOnCapturedContext: false);
 		}
 	}
 }

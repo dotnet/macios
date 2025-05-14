53aad700766d87e469f74a370293181ef56eba26
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-05-14 10:46:30 +0200

    [msbuild] Cleanup some legacy Xamarin logic.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/ComputeBundleLocation.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/ComputeBundleLocation.cs
@@ -311,7 +311,7 @@ namespace Xamarin.MacDev.Tasks {
 
 			// Assemblies and their related files
 			var assemblyDebugExtensions = new string [] {
-				".pdb", ".dll.mdb", ".exe.mdb",
+				".pdb",
 			};
 			foreach (var extension in assemblyDebugExtensions) {
 				if (filename.EndsWith (extension, StringComparison.OrdinalIgnoreCase))

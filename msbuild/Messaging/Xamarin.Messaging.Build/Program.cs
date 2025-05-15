78db2162d8aa6308261c74a314a8787ecf13a3d2
maagno <maagno@microsoft.com>
2025-05-14 23:54:43 -0400

    Added hack to support legacy Xamarin paths in Build agent

--- msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
+++ msbuild/Messaging/Xamarin.Messaging.Build/Program.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Messaging.Client;
+using System.IO;
 
 namespace Xamarin.Messaging.Build {
 	class Program {
@@ -10,6 +11,13 @@ namespace Xamarin.Messaging.Build {
 			var arguments = new AgentArgumentsParser ().ParseArguments (args);
 			var agent = new BuildAgent (topicGenerator, arguments.Version, arguments.VersionInfo);
 			var runner = new AgentConsoleRunner<BuildAgent> (agent, arguments);
+
+			//Hack to support legacy paths from Windows (likely Dev17 versions)
+			if (MessagingContext.BasePath.Contains ("Xamarin")) {
+				var xamarinPath = MessagingContext.BasePath.Substring (0, MessagingContext.BasePath.IndexOf ("Xamarin") + "Xamarin".Length);
+
+				MessagingContext.BuildsPath = Path.Combine (xamarinPath, "mtbs", "builds");
+			}
 
 			await runner.RunAsync (CancellationToken.None).ConfigureAwait (continueOnCapturedContext: false);
 		}

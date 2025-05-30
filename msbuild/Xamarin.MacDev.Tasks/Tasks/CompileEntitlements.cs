rolfbjarne/xamarin-macios

[msbuild] Enable signing by default for all platforms. Fixes #18469.
Enable signing by default for all platforms, because that's what Xcode does.

This fixes an issue for simulator builds, where certain features don't work unless
the app is signed (#18469).

It also simplifies and unifies our code to detect signing identities, so that it's
much more shared/equal beteween platforms.

However, simulator builds have a few peculiarities:

* The placeholder code signing certificate ("-", or what Xcode calls "Sign to
  Run Locally") is always used.
* Any entitlements the app requests will be embedded in the native executable in
  a Mach-O section named `__ents_der`.
* The actual app signature only demands the
  "com.apple.security.get-task-allow" entitlement (which seems to be allowed
  without a provisioning profile when using the placeholder code signing
  certificate).
* No provisioning profiles are used.

In order to provide a fairly decent way of restoring old behavior (not signing simulator
builds), I created a public property to determine whether we're building for the
simulator (SdkIsSimulator), and added documentation on how to use this new property
to disable code signing.

Fixes https://github.com/xamarin/xamarin-macios/issues/18469.

msbuild/Xamarin.MacDev.Tasks/Tasks/CompileEntitlements.cs
@@ -137,6 +137,21 @@ namespace Xamarin.MacDev.Tasks {
 			}
 		}
 
+		bool IsDeviceOrDesktop {
+			get {
+				switch (Platform) {
+				case ApplePlatform.iOS:
+				case ApplePlatform.TVOS:
+					return !SdkIsSimulator;
+				case ApplePlatform.MacOSX:
+				case ApplePlatform.MacCatalyst:
+					return true;
+				default:
+					throw new InvalidOperationException (string.Format (MSBStrings.InvalidPlatform, Platform));
+				}
+			}
+		}
+
 		PString MergeEntitlementString (PString pstr, MobileProvision? profile, bool expandWildcards, string? key)
 		{
 			string TeamIdentifierPrefix;
@@ -145,7 +160,7 @@ namespace Xamarin.MacDev.Tasks {
 			if (string.IsNullOrEmpty (pstr.Value))
 				return (PString) pstr.Clone ();
 
-			if (profile is null) {
+			if (profile is null && IsDeviceOrDesktop) {
 				if (!warnedTeamIdentifierPrefix && pstr.Value.Contains ("$(TeamIdentifierPrefix)")) {
 					Log.LogWarning (null, null, null, Entitlements, 0, 0, 0, 0, MSBStrings.W0108b /* Cannot expand $(TeamIdentifierPrefix) in Entitlements.plist without a provisioning profile for key '{0}' with value '{1}' */, key, pstr.Value);
 					warnedTeamIdentifierPrefix = true;
@@ -455,7 +470,7 @@ namespace Xamarin.MacDev.Tasks {
 			MobileProvision? profile;
 			PDictionary template;
 			PDictionary compiled;
-			PDictionary archived;
+			PDictionary? archived = null;
 			string path;
 
 			switch (SdkPlatform) {
@@ -507,34 +522,50 @@ namespace Xamarin.MacDev.Tasks {
 
 			compiled = GetCompiledEntitlements (profile, template);
 
-			ValidateAppEntitlements (profile, compiled);
-
-			archived = GetArchivedExpandedEntitlements (template, compiled);
-
-			try {
-				Directory.CreateDirectory (Path.GetDirectoryName (CompiledEntitlements!.ItemSpec));
-				WriteXcent (compiled, CompiledEntitlements.ItemSpec);
-			} catch (Exception ex) {
-				Log.LogError (MSBStrings.E0114, CompiledEntitlements, ex.Message);
-				return false;
-			}
-
-			SaveArchivedExpandedEntitlements (archived);
-
 			/* The path to the entitlements must be resolved to the full path, because we might want to reference it from a containing project that just references this project,
 			  * and in that case it becomes a bit complicated to resolve to a full path on disk when building remotely from Windows. Instead just resolve to a full path here,
 			  * and use that from now on. This has to be done from a task, so that we get the full path on the mac when executed remotely from Windows. */
-			var compiledEntitlementsFullPath = new TaskItem (Path.GetFullPath (CompiledEntitlements!.ItemSpec));
+			var compiledEntitlementsFullPath = Path.GetFullPath (CompiledEntitlements!.ItemSpec);
+			var compiledEntitlementsFullPathItem = new TaskItem (compiledEntitlementsFullPath);
 
-			if (Platform == Utils.ApplePlatform.MacCatalyst) {
-				EntitlementsInSignature = compiledEntitlementsFullPath;
-			} else if (SdkIsSimulator) {
-				if (compiled.Count > 0) {
-					EntitlementsInExecutable = compiledEntitlementsFullPath;
+			Directory.CreateDirectory (Path.GetDirectoryName (compiledEntitlementsFullPath));
+
+			if (SdkIsSimulator) {
+				// Any entitlements the app desires are stored inside the executable for simulator builds,
+				// and then the executable is signed with a placeholder signature ('-') + just a single
+				// entitlement (com.apple.security.get-task-allow). One consequence of storing entitlements
+				// this way is that no provisioning profile will be needed to sign the executable.
+				var simulatedEntitlements = compiled;
+				var simulatedXcent = Path.ChangeExtension (compiledEntitlementsFullPath, "").TrimEnd ('.') + "-Simulated.xcent";
+				try {
+					WriteXcent (simulatedEntitlements, simulatedXcent);
+				} catch (Exception ex) {
+					Log.LogError (MSBStrings.E0114, simulatedXcent, ex.Message);
+					return false;
 				}
+
+				EntitlementsInExecutable = new TaskItem (simulatedXcent);
+
+				// No matter what, I've only been able to make Xcode apply a single entitlement to simulator builds: com.apple.security.get-task-allow
+				compiled = new PDictionary ();
+				compiled.Add ("com.apple.security.get-task-allow", new PBoolean (true));
 			} else {
-				EntitlementsInSignature = compiledEntitlementsFullPath;
+				archived = GetArchivedExpandedEntitlements (template, compiled);
 			}
+
+			ValidateAppEntitlements (profile, compiled);
+
+			try {
+				WriteXcent (compiled, compiledEntitlementsFullPath);
+			} catch (Exception ex) {
+				Log.LogError (MSBStrings.E0114, compiledEntitlementsFullPathItem, ex.Message);
+				return false;
+			}
+
+			if (archived is not null)
+				SaveArchivedExpandedEntitlements (archived);
+
+			EntitlementsInSignature = compiledEntitlementsFullPathItem;
 
 			return !Log.HasLoggedErrors;
 		}

dotnet/macios

msbuild/Xamarin.MacDev.Tasks/Tasks/CompileEntitlements.cs
@@ -151,6 +151,21 @@ namespace Xamarin.MacDev.Tasks {
 			}
 		}
 
+		bool IsDeviceOrDesktop {
+			get {
+				switch (Platform) {
+				case ApplePlatform.iOS:
+				case ApplePlatform.TVOS:
+					return !SdkIsSimulator;
+				case ApplePlatform.MacOSX:
+				case ApplePlatform.MacCatalyst:
+					return true;
+				default:
+					throw new InvalidOperationException (string.Format (MSBStrings.InvalidPlatform, Platform));
+				}
+			}
+		}
+
 		PString MergeEntitlementString (PString pstr, MobileProvision? profile, bool expandWildcards, string? key)
 		{
 			string TeamIdentifierPrefix;
@@ -159,7 +174,7 @@ namespace Xamarin.MacDev.Tasks {
 			if (string.IsNullOrEmpty (pstr.Value))
 				return (PString) pstr.Clone ();
 
-			if (profile is null) {
+			if (profile is null && IsDeviceOrDesktop) {
 				if (!warnedTeamIdentifierPrefix && pstr.Value.Contains ("$(TeamIdentifierPrefix)")) {
 					Log.LogWarning (null, null, null, Entitlements, 0, 0, 0, 0, MSBStrings.W0108b /* Cannot expand $(TeamIdentifierPrefix) in Entitlements.plist without a provisioning profile for key '{0}' with value '{1}' */, key, pstr.Value);
 					warnedTeamIdentifierPrefix = true;
@@ -537,32 +552,50 @@ namespace Xamarin.MacDev.Tasks {
 
 			compiled = GetCompiledEntitlements (profile, templates);
 
-			ValidateAppEntitlements (profile, compiled);
-
-			archived = GetArchivedExpandedEntitlements (templates, compiled);
-
-			try {
-				Directory.CreateDirectory (Path.GetDirectoryName (CompiledEntitlements!.ItemSpec));
-				WriteXcent (compiled, CompiledEntitlements.ItemSpec);
-			} catch (Exception ex) {
-				Log.LogError (MSBStrings.E0114, CompiledEntitlements, ex.Message);
-				return false;
-			}
-
-			SaveArchivedExpandedEntitlements (archived);
-
 			/* The path to the entitlements must be resolved to the full path, because we might want to reference it from a containing project that just references this project,
 			  * and in that case it becomes a bit complicated to resolve to a full path on disk when building remotely from Windows. Instead just resolve to a full path here,
 			  * and use that from now on. This has to be done from a task, so that we get the full path on the mac when executed remotely from Windows. */
-			var compiledEntitlementsFullPath = new TaskItem (Path.GetFullPath (CompiledEntitlements!.ItemSpec));
+			var compiledEntitlementsFullPath = Path.GetFullPath (CompiledEntitlements!.ItemSpec);
+			var compiledEntitlementsFullPathItem = new TaskItem (compiledEntitlementsFullPath);
+
+			Directory.CreateDirectory (Path.GetDirectoryName (compiledEntitlementsFullPath));
 
 			if (BundleEntitlementsInExecutable) {
-				if (compiled.Count > 0) {
-					EntitlementsInExecutable = compiledEntitlementsFullPath;
+				// Any entitlements the app desires are stored inside the executable for simulator builds,
+				// and then the executable is signed with a placeholder signature ('-') + just a single
+				// entitlement (com.apple.security.get-task-allow). One consequence of storing entitlements
+				// this way is that no provisioning profile will be needed to sign the executable.
+				var simulatedEntitlements = compiled;
+				var simulatedXcent = Path.ChangeExtension (compiledEntitlementsFullPath, "").TrimEnd ('.') + "-Simulated.xcent";
+				try {
+					WriteXcent (simulatedEntitlements, simulatedXcent);
+				} catch (Exception ex) {
+					Log.LogError (MSBStrings.E0114, simulatedXcent, ex.Message);
+					return false;
 				}
+
+				EntitlementsInExecutable = new TaskItem (simulatedXcent);
+
+				// No matter what, I've only been able to make Xcode apply a single entitlement to simulator builds: com.apple.security.get-task-allow
+				compiled = new PDictionary ();
+				compiled.Add ("com.apple.security.get-task-allow", new PBoolean (true));
 			} else {
-				EntitlementsInSignature = compiledEntitlementsFullPath;
+				archived = GetArchivedExpandedEntitlements (templates, compiled);
 			}
+
+			ValidateAppEntitlements (profile, compiled);
+
+			try {
+				WriteXcent (compiled, compiledEntitlementsFullPath);
+			} catch (Exception ex) {
+				Log.LogError (MSBStrings.E0114, compiledEntitlementsFullPathItem, ex.Message);
+				return false;
+			}
+
+			if (archived is not null)
+				SaveArchivedExpandedEntitlements (archived);
+
+			EntitlementsInSignature = compiledEntitlementsFullPathItem;
 
 			return !Log.HasLoggedErrors;
 		}
@@ -980,7 +1013,7 @@ namespace Xamarin.MacDev.Tasks {
 			}
 		}
 
-		void ValidateTypeOrArray<T, M> (MobileProvision? profile, EntitlementInfo info, bool onlyWarn, string key, KeyValuePair<string?, PObject> kvp, string? provisioningProfileName, PDictionary? provisioningEntitlements, string requiredType)
+		void ValidateTypeOrArray<T, M> (MobileProvision? profile, EntitlementInfo info, bool onlyWarn, string key, KeyValuePair<string, PObject> kvp, string? provisioningProfileName, PDictionary? provisioningEntitlements, string requiredType)
 			where T : PObject
 		{
 			// entitlement is a boolean, provisioning profile has the entitlement with either a string or an array of strings of valid values for the entitlement
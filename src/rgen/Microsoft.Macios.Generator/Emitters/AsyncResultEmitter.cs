e6150894733e1dfed0d9dfebd71b8acb56a0de4a
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-06-27 11:51:59 -0400

    [RGen] Provide a new code emitter for the async result types.
    This commit does the following:
    
    1. Creates a new provider that will retrieve all the async result types
       to generate.
    2. Registers a new code generator that will generate the needed result
       types for the async methods.
    3. Provides a async result emitter that creates the result types by
       using the parameters of the completion handlers.
       a. Calculates all the needed namespaces for the arguments.
       b. Creates the async result class.
       c. Provides properties to access the result type data using the
       parameter names.
       d. Creates a constructor that get the same parameters as the
       completion handlers.
    4. Adds the avility to state in the tests that more files will be
       generated and set their contents expectation.

---
+++ src/rgen/Microsoft.Macios.Generator/Emitters/AsyncResultEmitter.cs
@@ -0,0 +1,93 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.Macios.Generator.Availability;
+using Microsoft.Macios.Generator.DataModel;
+using Microsoft.Macios.Generator.Extensions;
+using Microsoft.Macios.Generator.Formatters;
+using Microsoft.Macios.Generator.IO;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Microsoft.Macios.Generator.Emitters;
+
+class AsyncResultEmitter (
+	TabbedStringBuilder builder) {
+	
+	public bool TryEmit (AsyncResultInfo asyncResult,  [NotNullWhen (false)] out ImmutableArray<Diagnostic>? diagnostics)
+	{
+		diagnostics = null;
+		
+		// get the parameters based on the completion handler
+		if (asyncResult.CompletionHandler.Type.Delegate is null) {
+			diagnostics = [Diagnostic.Create (
+				Diagnostics
+					.RBI0000, // An unexpected error occurred while processing '{0}'. Please fill a bug report at https://github.com/dotnet/macios/issues/new.
+				null,
+				asyncResult.FullyQualifiedName)];
+			return false;
+		}
+		// check if we should include the last parameter or not
+		var parameters = asyncResult.CompletionHandler.Type.Delegate.Parameters
+			.Select (p => p.ToParameter ())
+			.ToImmutableArray ();
+		if (parameters.Length > 0 && parameters [^1].Type.Name.Contains ("NSError")) {
+			parameters = parameters [..^1];
+		}
+
+		var typeNamespace = string.Join ('.', asyncResult.Namespace);
+		// collect the namespaces of the parameters of the delegate, use a set to avoid duplicates
+		var namespaces = new HashSet<string> ();
+		foreach (var parameter in asyncResult.CompletionHandler.Type.Delegate.Parameters) {
+			var ns = string.Join ('.', parameter.Type.Namespace);
+			if (!string.IsNullOrEmpty (ns) 
+			    && ns != typeNamespace  // make use we do not add a using for the same namespace
+			    && namespaces.Add (ns)) {
+				builder.WriteLine($"using {ns};");
+			}
+		}
+		
+		// add space for readability
+		if (namespaces.Count > 0)
+			builder.WriteLine ();
+
+		// namespace declaration
+		builder.WriteLine ($"namespace {typeNamespace};");
+		builder.WriteLine ();
+
+		using (var classBlock = 
+		       builder.CreateBlock ($"public partial class {asyncResult.Name}", true)) {
+			
+			foreach (var parameter in parameters) {
+				classBlock.WriteLine ();
+				classBlock.WriteLine ($"public {parameter.Type.GetIdentifierSyntax ().ToString ()} {parameter.Name.Capitalize ()} {{ get; set; }}");
+			}
+
+			// get the parameter from the delegate type
+			classBlock.WriteLine ();
+			classBlock.WriteLine ("partial void Initialize ();");
+			
+			// create a constructor for the result type 
+			var constructor = new Constructor (
+				type: asyncResult.Name, 
+				symbolAvailability: new SymbolAvailability(), 
+				attributes: [], 
+				modifiers: [Token (SyntaxKind.PublicKeyword)], 
+				parameters: parameters);
+			classBlock.WriteLine ();
+			using (var constructorBlock = classBlock.CreateBlock ($"{constructor.ToDeclaration ()}", true)) {
+				foreach (var parameter in parameters) {
+					constructorBlock.WriteLine ($"this.{parameter.Name.Capitalize ()} = {parameter.Name};");
+				}
+				constructorBlock.WriteLine ("Initialize ();");
+			}
+		}
+
+		return true;
+	}
+}

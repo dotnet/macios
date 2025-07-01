1db1d20ebca91fc47fcf8b6b88758c4d8af53960
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-07-01 13:03:46 -0400

    Remov enot needed namespace.

--- tests/rgen/Microsoft.Macios.Generator.Tests/TestDataFactory.cs
+++ tests/rgen/Microsoft.Macios.Generator.Tests/TestDataFactory.cs
@@ -5,7 +5,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.Macios.Generator.DataModel;
 using Microsoft.Macios.Generator.Formatters;

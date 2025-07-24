4524e906762062005200dfd82e7627f71d812065
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-07-24 18:05:24 -0400

    Clean.

--- src/rgen/Microsoft.Macios.Generator/DataModel/ImmutableArrayEqualityComparer.cs
+++ src/rgen/Microsoft.Macios.Generator/DataModel/ImmutableArrayEqualityComparer.cs
@@ -4,8 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using Microsoft.CodeAnalysis;
 
 namespace Microsoft.Macios.Generator.DataModel;
 

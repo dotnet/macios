9455c0b41665a93ba2e86fecc98fc623a125a1e9
Martin Løbger <martin@lobger.com>
2025-09-04 11:14:32 +0200

    Type data is not supported to read from property list

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetPropertyListValue.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetPropertyListValue.cs
@@ -81,7 +81,7 @@ namespace Xamarin.MacDev.Tasks {
 				i++;
 			} while (i < path.Length);
 
-			if (array is not null || dict is not null) {
+			if (array is not null || dict is not null || value.Type is PObjectType.Data) {
 				Log.LogError (MSBStrings.E0157, value.Type.ToString ().ToLowerInvariant ());
 				return false;
 			}

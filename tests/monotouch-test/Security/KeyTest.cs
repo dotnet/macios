85b41cd397e898f35ebbb27435fa48ac518cb368
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-10-14 14:36:50 +0200

    x64 only

--- tests/monotouch-test/Security/KeyTest.cs
+++ tests/monotouch-test/Security/KeyTest.cs
@@ -297,6 +297,9 @@ namespace MonoTouchFixtures.Security {
 		[Test]
 		public void SignVerifyRSAMinPKCS1SHA1 ()
 		{
+			if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+				Assert.Ignore ("This test raises a SIGFPE signal which ends up killing the process on x64.");
+
 			SecKey private_key;
 			SecKey public_key;
 			var label = $"KeyTest.SignVerifyRSAMinPKCS1SHA1-{CFBundle.GetMain ().Identifier}-{GetType ().FullName}-{Process.GetCurrentProcess ().Id}";

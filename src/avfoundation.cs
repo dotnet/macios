22af87e390c8f8f02a083ebaf775e26239b0e102
Alex Soto <alex@soto.dev>
2025-11-19 15:37:01 -0500

    [xcode26.2] Update AVFoundation bindings to Xcode 26.2 Beta 2

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -18685,6 +18685,18 @@ namespace AVFoundation {
 		[TV (26, 0), MacCatalyst (26, 0), Mac (26, 0), iOS (26, 0)]
 		[Export ("cameraLensSmudgeDetectionStatus")]
 		AVCaptureCameraLensSmudgeDetectionStatus CameraLensSmudgeDetectionStatus { get; }
+
+		// From the AVCaptureDeviceEdgeLight (AVCaptureDevice) category
+
+		[TV (26, 2), MacCatalyst (26, 2), Mac (26, 2), iOS (26, 2)]
+		[Static]
+		[Export ("edgeLightEnabled")]
+		bool EdgeLightEnabled { [Bind ("isEdgeLightEnabled")] get; }
+
+		[TV (26, 2), MacCatalyst (26, 2), Mac (26, 2), iOS (26, 2)]
+		[Static]
+		[Export ("edgeLightActive")]
+		bool EdgeLightActive { [Bind ("isEdgeLightActive")] get; }
 	}
 
 	[TV (26, 0), MacCatalyst (26, 0), Mac (26, 0), iOS (26, 0)]
@@ -19091,6 +19103,10 @@ namespace AVFoundation {
 		[NoTV, NoMacCatalyst, NoMac, iOS (26, 0)]
 		[Export ("smartFramingSupported")]
 		bool SmartFramingSupported { [Bind ("isSmartFramingSupported")] get; }
+
+		[TV (26, 2), MacCatalyst (26, 2), Mac (26, 2), iOS (26, 2)]
+		[Export ("edgeLightSupported")]
+		bool EdgeLightSupported { [Bind ("isEdgeLightSupported")] get; }
 	}
 
 	[TV (26, 0), MacCatalyst (26, 0), Mac (26, 0), iOS (26, 0)]

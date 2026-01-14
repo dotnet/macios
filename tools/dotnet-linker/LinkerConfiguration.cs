rolfbjarne/xamarin-macios

[dotnet] Add ReadyToRun support for CoreCLR and update related configurations
- Introduced ReadyToRun configuration in Xamarin.Shared.Sdk.props.
- Updated bundler environment variables in Xamarin.Shared.Sdk.targets to support ReadyToRun.
- Added host_runtime_contract.h to the build process in Makefile.
- Updated coreclr-bridge.m and runtime.m for R2R image handling and logging.
- Updated runtime.h to reflect changes in the xamarin_pinvoke_override function signature.
- Adjusted how the native main function is generated to load the RTR_HEADER symbol when using ReadyToRun.

tools/dotnet-linker/LinkerConfiguration.cs
@@ -271,6 +271,13 @@ namespace Xamarin.Linker {
 				case "PlatformAssembly":
 					PlatformAssembly = Path.GetFileNameWithoutExtension (value);
 					break;
+				case "PublishReadyToRun":
+					if (!string.IsNullOrEmpty (value)) {
+						if (!TryParseOptionalBoolean (value, out var publish_ready_to_run))
+							throw new InvalidOperationException ($"Unable to parse the {key} value: {value} in {linker_file}");
+						Application.PublishReadyToRun = publish_ready_to_run;
+					}
+					break;
 				case "ReferenceNativeSymbol": {
 					(string symbolType, string symbolMode, string symbol) = SplitString3 (value, ':');
 					var mode = SymbolMode.Default;

dotnet/macios

tools/dotnet-linker/LinkerConfiguration.cs
@@ -271,6 +271,13 @@ namespace Xamarin.Linker {
 				case "PlatformAssembly":
 					PlatformAssembly = Path.GetFileNameWithoutExtension (value);
 					break;
+				case "PublishReadyToRun":
+					if (!string.IsNullOrEmpty (value)) {
+						if (!TryParseOptionalBoolean (value, out var publishReadyToRun))
+							throw new InvalidOperationException ($"Unable to parse the {key} value: {value} in {linker_file}");
+						Application.PublishReadyToRun = publishReadyToRun;
+					}
+					break;
 				case "ReferenceNativeSymbol": {
 					(string symbolType, string symbolMode, string symbol) = SplitString3 (value, ':');
 					var mode = SymbolMode.Default;
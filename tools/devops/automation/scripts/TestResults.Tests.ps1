abe9614f8851937b02157e395462b980397b5e34
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-01-16 10:06:58 +0100

    [devops] Fix a few issues with pwsh tests and test result parsing.
    * Fix an issue parsing test results, causing errors like:
    
        Failed to compute test summaries: Method invocation failed because [System.Char] does not contain a method named 'Contains'.
    
    * Fix tests related to checking if a commit is the last in a PR.

--- tools/devops/automation/scripts/TestResults.Tests.ps1
+++ tools/devops/automation/scripts/TestResults.Tests.ps1
@@ -915,7 +915,7 @@ Describe "TestResults tests" {
 
 <summary>1 tests failed, 0 tests passed.</summary>
 <details>
-
+# :tada: All 5 tests passed :tada:
 </details>
 
 [Html Report (VSDrops)](vsdropsIndex/testStagedotnettests_maccatalyst-1/;/tests/vsdrops_index.html) [Download](/_apis/build/builds//artifacts?artifactName=HtmlReport-testStagedotnettests_maccatalyst-1&api-version=6.0&`$format=zip)
@@ -970,9 +970,8 @@ Describe "TestResults tests" {
 
 ### :x: dotnettests tests (MacCatalyst)
 
-<summary>5 tests failed, 6 tests passed.</summary>
 <details>
-
+<summary>5 tests failed, 6 tests passed.</summary>
 </details>
 
 [Html Report (VSDrops)](vsdropsIndex/testStagedotnettests_maccatalyst-1/;/tests/vsdrops_index.html) [Download](/_apis/build/builds//artifacts?artifactName=HtmlReport-testStagedotnettests_maccatalyst-1&api-version=6.0&`$format=zip)

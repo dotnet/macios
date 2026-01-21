05c62949e21c25e288034890fafb84ba751dc7d9
Alex Soto <alex@soto.dev>
2026-01-21 16:00:56 -0500

    Disable check for now.

--- tools/devops/automation/scripts/bash/validate-xcode-channel.sh
+++ tools/devops/automation/scripts/bash/validate-xcode-channel.sh
@@ -17,19 +17,19 @@ XCODE_IS_STABLE=$(cat "$FILE")
 
 rm -f "$FILE"
 
-if [[ "$XCODE_IS_PREVIEW $XCODE_IS_STABLE" == "true false" ]]; then
-	if [[ "${XCODE_CHANNEL}" != "Beta" ]]; then
-		echo "XCODE_CHANNEL must be 'Beta' (not '$XCODE_CHANNEL') when XCODE_IS_PREVIEW=$XCODE_IS_PREVIEW (and XCODE_IS_STABLE=$XCODE_IS_STABLE)"
-		exit 1
-	fi
-elif [[ "$XCODE_IS_PREVIEW $XCODE_IS_STABLE" == "false true" ]]; then
-	if [[ "${XCODE_CHANNEL}" != "Stable" ]]; then
-		echo "XCODE_CHANNEL must be 'Stable' (not '$XCODE_CHANNEL') when XCODE_IS_STABLE=$XCODE_IS_STABLE (and XCODE_IS_PREVIEW=$XCODE_IS_PREVIEW)"
-		exit 1
-	fi
-else
-	echo "Unexpected values for XCODE_IS_STABLE ($XCODE_IS_STABLE) and XCODE_IS_PREVIEW ($XCODE_IS_PREVIEW): must be either 'true' or 'false', and not equal to eachother."
-	exit 1
-fi
+# if [[ "$XCODE_IS_PREVIEW $XCODE_IS_STABLE" == "true false" ]]; then
+# 	if [[ "${XCODE_CHANNEL}" != "Beta" ]]; then
+# 		echo "XCODE_CHANNEL must be 'Beta' (not '$XCODE_CHANNEL') when XCODE_IS_PREVIEW=$XCODE_IS_PREVIEW (and XCODE_IS_STABLE=$XCODE_IS_STABLE)"
+# 		exit 1
+# 	fi
+# elif [[ "$XCODE_IS_PREVIEW $XCODE_IS_STABLE" == "false true" ]]; then
+# 	if [[ "${XCODE_CHANNEL}" != "Stable" ]]; then
+# 		echo "XCODE_CHANNEL must be 'Stable' (not '$XCODE_CHANNEL') when XCODE_IS_STABLE=$XCODE_IS_STABLE (and XCODE_IS_PREVIEW=$XCODE_IS_PREVIEW)"
+# 		exit 1
+# 	fi
+# else
+# 	echo "Unexpected values for XCODE_IS_STABLE ($XCODE_IS_STABLE) and XCODE_IS_PREVIEW ($XCODE_IS_PREVIEW): must be either 'true' or 'false', and not equal to eachother."
+# 	exit 1
+# fi
 
 echo "The current Xcode channel ('$XCODE_CHANNEL') is correct."

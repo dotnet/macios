fe71b2cc13f72d4e500393099660a32b91c138cf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-02-02 17:56:27 +0100

    [msbuild] Fix listing devices with no udid. Fixes #24605.
    In some cases, `devicectl` may list a device with no udid, so handle this by
    using the 'identifier' value as the udid.
    
    Fixes this problem:
    
    ```
    error MSB4028: The "GetAvailableDevices" task's outputs could not be retrieved from the "DiscardedDevices" parameter. Parameter "includeEscaped" cannot have zero length.
    ```
    
    Fixes https://github.com/dotnet/macios/issues/24605.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetAvailableDevices.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetAvailableDevices.cs
@@ -31,6 +31,9 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 
 	CancellationTokenSource? cancellationTokenSource;
 
+	public string DeviceCtlFile { get; set; } = "";
+	public string SimCtlFile { get; set; } = "";
+
 	public override bool Execute ()
 	{
 		if (ShouldExecuteRemotely ())
@@ -149,6 +152,10 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 
 	protected virtual async System.Threading.Tasks.Task<string> ExecuteCtlAsync (params string [] args)
 	{
+		var file = args [0] == "devicectl" ? DeviceCtlFile : SimCtlFile;
+		if (File.Exists (file))
+			return File.ReadAllText (file);
+
 		var tmpfile = Path.GetTempFileName ();
 		try {
 			var arguments = new List<string> (args) {
@@ -180,6 +187,7 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 			foreach (var device in array) {
 				var name = device.GetStringPropertyOrEmpty ("deviceProperties", "name");
 				var udid = device.GetStringPropertyOrEmpty ("hardwareProperties", "udid");
+				var identifier = device.GetStringPropertyOrEmpty ("identifier");
 
 				var deviceProperties = device.GetNullableProperty ("deviceProperties");
 				var buildVersion = deviceProperties.GetStringPropertyOrEmpty ("osBuildUpdate");
@@ -199,6 +207,12 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 				var connectionProperties = device.GetNullableProperty ("connectionProperties");
 				var transportType = connectionProperties.GetStringPropertyOrEmpty ("transportType");
 				var pairingState = connectionProperties.GetStringPropertyOrEmpty ("pairingState");
+
+				if (string.IsNullOrEmpty (udid))
+					udid = identifier;
+
+				if (string.IsNullOrEmpty (udid))
+					udid = $"<unknown udid #{rv.Count + 1}>";
 
 				var item = new TaskItem (udid);
 				item.SetMetadata ("Name", name);

364ca9fccb26f037d3d2fbea38ab2dde7d386067
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-02-06 09:01:55 +0100

    Try restricting sharpie+xtro to arm64.

--- tools/devops/automation/scripts/TestConfiguration.psm1
+++ tools/devops/automation/scripts/TestConfiguration.psm1
@@ -30,6 +30,7 @@ class TestConfiguration {
             $splitByPlatforms = $config.splitByPlatforms
             $testPrefix = $config.testPrefix
             $testStage = $config.testStage ? $config.testStage : $config.testPrefix
+            $extraBotDemand = $config.extraBotDemand
 
             if ($this.stageFilter) {
                 if ($testStage -eq $this.stageFilter) {
@@ -47,6 +48,7 @@ class TestConfiguration {
             $vars["LABEL"] = $label
             $vars["TESTS_LABELS"] = "$($this.testsLabels),run-$($label)-tests"
             $vars["TEST_STAGE"] = $testStage
+            $vars["EXTRA_BOT_DEMAND"] = $extraBotDemand
 
             if ($splitByPlatforms -eq "True") {
                 if ($enabledPlatformsForConfig.Length -eq 0) {

e085c4c7ea8401128806d97dba18ec341a5761cb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-04-17 19:42:20 +0200

    Fix build.

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ACToolTaskTest.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ACToolTaskTest.cs
@@ -788,7 +788,7 @@ namespace Xamarin.MacDev.Tasks {
 			ExecuteTask (actool, 1);
 
 			var errorMessages = Engine.Logger.ErrorEvents.Select (e => e.Message).ToList ();
-			Assert.IsTrue (errorMessages.Any (m => m.Contains ("Can't find the AppIcon 'InexistentIcon'")),
+			Assert.IsTrue (errorMessages.Any (m => m?.Contains ("Can't find the AppIcon 'InexistentIcon'") == true),
 				"Should report that InexistentIcon is not found among image resources");
 		}
 

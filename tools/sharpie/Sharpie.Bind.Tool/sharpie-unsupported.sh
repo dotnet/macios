ca834013b10a09beef4698415d20c4c086bfa750
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-05-14 09:10:45 +0200

    I like this text better.

--- tools/sharpie/Sharpie.Bind.Tool/sharpie-unsupported.sh
+++ tools/sharpie/Sharpie.Bind.Tool/sharpie-unsupported.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo "error: sharpie is not supported with an x64 runtime." >&2
-echo "" >&2
-echo "sharpie requires Apple's libclang, which is only available for arm64." >&2
-echo "Please install the arm64 version of .NET and run sharpie with the arm64 .NET runtime." >&2
+echo "error: error: sharpie is not supported on x64. Please use an Apple Silicon (arm64) Mac." >&2
 exit 1

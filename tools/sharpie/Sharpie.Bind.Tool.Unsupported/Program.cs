9cf6e2a794b3e55961ccefdd80520e64f597b59e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-05-14 09:01:48 +0200

    I like this text better. Much simpler.

--- tools/sharpie/Sharpie.Bind.Tool.Unsupported/Program.cs
+++ tools/sharpie/Sharpie.Bind.Tool.Unsupported/Program.cs
@@ -4,10 +4,7 @@
 class Program {
 	static int Main (string [] args)
 	{
-		Console.Error.WriteLine ("error: sharpie is not supported with an x64 runtime.");
-		Console.Error.WriteLine ();
-		Console.Error.WriteLine ("sharpie requires Apple's libclang, which is only available for arm64.");
-		Console.Error.WriteLine ("Please install the arm64 version of .NET and run sharpie with the arm64 .NET runtime.");
+		Console.Error.WriteLine ("error: sharpie is not supported on x64. Please use an Apple Silicon (arm64) Mac.");
 		return 1;
 	}
 }

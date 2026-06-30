a68c5c0bcab780d59dc5e95bf02d477e5c413739
Rolf Bjarne Kvinge <rokvin@microsoft.com>
2026-06-30 13:15:18 +0200

    [net11.0] [dotnet] Set DOTNET_MODIFIABLE_ASSEMBLIES environment variable for CoreCLR (#25870)
    When using the interpreter with CoreCLR runtime, set `DOTNET_MODIFIABLE_ASSEMBLIES=debug` environment variable in the generated `main.m`. This enables hot reload and modifiable assemblies support for CoreCLR on iOS.
    
    Cherry-picked from thaystg/xamarin-macios@38db4b5899 (dev/thays/set_modifiable_assemblies_env_var).
    
    Recreation of #25864 (from @matouskozak) beacuse we can't build PRs from forks.
    
    ---------
    
    Co-authored-by: Thays Grazia <thtaglia@microsoft.com>

--- tools/dotnet-linker/Steps/GenerateMainStep.cs
+++ tools/dotnet-linker/Steps/GenerateMainStep.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
+using Xamarin.Bundler;
 using Xamarin.Linker;
 
 #nullable enable
@@ -32,7 +33,7 @@ namespace Xamarin {
 			contents.AppendLine ();
 			contents.AppendLine ("static void xamarin_initialize_dotnet ()");
 			contents.AppendLine ("{");
-			if (Configuration.Application.PackageManagedDebugSymbols && Configuration.Application.UseInterpreter)
+			if (Configuration.Application.PackageManagedDebugSymbols && (Configuration.Application.UseInterpreter || Configuration.Application.XamarinRuntime == XamarinRuntime.CoreCLR))
 				contents.AppendLine ($"\tsetenv (\"DOTNET_MODIFIABLE_ASSEMBLIES\", \"debug\", 1);");
 			contents.AppendLine ("}");
 			contents.AppendLine ();

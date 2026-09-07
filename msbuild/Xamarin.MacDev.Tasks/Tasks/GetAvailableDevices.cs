5c88c6aa78ce6de8dcda85435e66085203ef64d4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2026-09-07 19:43:48 +0200

    [msbuild] Discard any simulators we get from devicectl in the GetAvailableDevices task.
    In Xcode 27+, `devicectl` returns simulators too. Discard any simulators we get from `devicectl` (the json output from `devicectl` doesn't contain all the information we need) that matches an existing simulator we got from `simctl`.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetAvailableDevices.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetAvailableDevices.cs
@@ -45,8 +45,11 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 		System.Threading.Tasks.Task.WhenAll (new [] { devicectlTask, simctlTask }).Wait (cancellationTokenSource.Token);
 
 		var devices = new List<DeviceInfo> ();
-		devices.AddRange (devicectlTask.Result);
 		devices.AddRange (simctlTask.Result);
+
+		// devicectl may return simulators as well, so exclude any devices we've already added
+		var devicectlDevices = devicectlTask.Result;
+		devices.AddRange (devicectlDevices.Where (dev => !devices.Any (sim => sim.Udid == dev.Udid)));
 
 		// filter to the current platform
 		foreach (var d in devices.Where (d => !d.Discarded && d.Platform != Platform))
@@ -133,15 +136,17 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 
 	class DeviceInfo {
 		public ITaskItem Item { get; set; }
+		public string Udid { get; set; }
 		public IEnumerable<string> RuntimeIdentifiers { get; set; }
 		public ApplePlatform Platform { get; set; }
 		public IPhoneDeviceType DeviceType { get; set; }
 		public Version MinimumOSVersion { get; set; }
 		public string DiscardedReason { get; set; }
 		public bool Discarded { get => !string.IsNullOrEmpty (DiscardedReason); }
-		public DeviceInfo (ITaskItem item, IEnumerable<string> runtimeIdentifiers, ApplePlatform platform, IPhoneDeviceType deviceType, Version minimumOSVersion, string discardedReason)
+		public DeviceInfo (ITaskItem item, string udid, IEnumerable<string> runtimeIdentifiers, ApplePlatform platform, IPhoneDeviceType deviceType, Version minimumOSVersion, string discardedReason)
 		{
 			Item = item;
+			Udid = udid;
 			RuntimeIdentifiers = runtimeIdentifiers;
 			Platform = platform;
 			DeviceType = deviceType;
@@ -252,7 +257,7 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 
 			Version.TryParse (device.OSVersion, out var minimumOSVersion);
 
-			rv.Add (new DeviceInfo (item, [runtimeIdentifier], platform, deviceType, minimumOSVersion ?? new Version (0, 0), discardedReason));
+			rv.Add (new DeviceInfo (item, udid, [runtimeIdentifier], platform, deviceType, minimumOSVersion ?? new Version (0, 0), discardedReason));
 		}
 		return rv;
 	}
@@ -390,7 +395,7 @@ public class GetAvailableDevices : XamarinTask, ICancelableTask {
 				}
 			}
 
-			rv.Add (new DeviceInfo (item, runtimeIdentifiers, platform, deviceType, minimumOSVersion, discardedReason));
+			rv.Add (new DeviceInfo (item, device.Udid, runtimeIdentifiers, platform, deviceType, minimumOSVersion, discardedReason));
 		}
 		return rv;
 	}
